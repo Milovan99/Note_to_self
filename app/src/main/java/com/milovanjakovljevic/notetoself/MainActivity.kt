d243a989be3f8efa9137d7ebd7d5831e915f71a8
Milovan99 <milovanjakovljevic78@gmail.com>
2023-02-01 17:59:47 +0100

    Izmena dizajna

--- app/src/main/java/com/milovanjakovljevic/notetoself/MainActivity.kt
+++ app/src/main/java/com/milovanjakovljevic/notetoself/MainActivity.kt
@@ -14,6 +14,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import androidx.core.graphics.toColor
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
